c7967e7027c032a60c2e68ab4d9e2b596c620b89
Zane97 <zanejvv1997@gmail.com>
2020-10-02 21:22:19 +0200

    Inital attempt at repeate string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, count) {
+    var out = "";
+    
+    for(; count > 0; count--){
+            out += phrase;
+    }
+    
+    return out;
 }
 
 module.exports = repeatString
