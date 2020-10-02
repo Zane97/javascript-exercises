92864c04b6812d48bb5968667513f8aa5c01ffb3
Zane97 <zanejvv1997@gmail.com>
2020-10-02 21:25:03 +0200

    Added an error check for negitive numbers

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function(phrase, count) {
     var out = "";
     
+    // Error checking
+    if (count < 0)
+        return "ERROR";
+    
     for(; count > 0; count--){
             out += phrase;
     }
